9b6be0d2d4e439657ccd210c8802dc8e953b751a
shahihku <121048524+shahihku@users.noreply.github.com>
2023-01-12 04:32:16 +0700

    Update oh.sh

--- 2.2/tambah/oh.sh
+++ 2.2/tambah/oh.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd
-wget https://raw.githubusercontent.com/shahihku/tambal/main/2.2/lengkap/oh.zip
+wget https://raw.githubusercontent.com/shahihku/tambal/main/2.2/tambah/oh.zip
 unzip oh.zip
 cd /root/bin
 chmod +x *
